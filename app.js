21e49ae529d930a84e7b7cae0803e377f1d36bf8
Travisturner09 <tnadekunle@gmail.com>
2022-04-14 16:16:30 +0100

    assignment

---
+++ app.js
@@ -0,0 +1,9 @@
+var Fname = "Tony";
+var Sname = "Adekunle";
+
+var firstname = Fname.slice(0,1);
+var Surname = Sname.slice(0,8);
+var Userid = prompt("Input id")
+output = "Welcome Back " + firstname + Surname;
+
+alert(output);
