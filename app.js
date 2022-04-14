aa9bedb46aa3bf4170fa22e9f29d5de385075147
Travisturner09 <tnadekunle@gmail.com>
2022-04-14 16:46:58 +0100

    ammendment

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var Fname = "Tony";
-var Sname = "Adekunle";
+var Fname = "tony";
+var Sname = "adekunle";
 
 var firstname = Fname.slice(0,1);
-var Surname = Sname.slice(0,8);
+var Surname = Sname.slice(1,8);
 var Userid = prompt("Input id")
 output = "Welcome Back " + firstname + Surname;
-
 alert(output);
