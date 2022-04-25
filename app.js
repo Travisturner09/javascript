09f4793c6255849f7879df436f1981344c6a4421
Travisturner09 <tnadekunle@gmail.com>
2022-04-25 20:10:52 +0100

    love

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var Fname = "tony";
 var Sname = "adekunle";
 
-var firstname = Fname.slice(0,1);
-var Surname = Sname.slice(1,8);
-var Userid = prompt("Input id")
-output = "Welcome Back " + firstname + Surname;
-alert(output);
+var firstinput("input your name");
+var Secondinput("input your partner name");
+
+function lovecal
